b98c7e76bba93d2605b7ec0b23cc1d92967600c1
Alexander Moskalev <amos4art@gmail.com>
2017-11-25 18:23:03 -0500

    added solution for task 8

--- part_b/hw1.rkt
+++ part_b/hw1.rkt
@@ -21,26 +21,43 @@
                 (if (= fnum n )
                     (cons (car (fstream)) null)
                     (cons (car (fstream)) (f (cdr (fstream)) (+ fnum 1)))))])
-        (f s 1)))
+    (f s 1)))
 
 
-( define funny-number-stream
+(define funny-number-stream
    (letrec ([convert (lambda (y) (if (= 0 (remainder y 5)) (- 0 y) y))]
             [f (lambda (x) (cons (convert x) (lambda () (f (+ x 1)))))])
-    (lambda () (f 1)))) 
+     (lambda () (f 1)))) 
 
-( define dan-then-dog
+(define dan-then-dog
    (letrec ([f (lambda (x) (if (= 0 (remainder x 2))
                                (cons "dog.jpg" (lambda () (f (+ x 1))))
                                (cons "dan.jpg" (lambda () (f (+ x 1))))))])
-     (lambda () (f 1)))
-)
+     (lambda () (f 1))))
 
 
 (define (stream-add-zero s)
   (letrec ([f (lambda (fstream)
                     (cons (cons 0 (car (fstream))) (stream-add-zero (cdr (fstream)))))])
-        ( lambda () (f s))))
+    (lambda () (f s))))
+
+
+(define (cycle-lists-2 xs ys)
+  (letrec ([f (lambda (xlist ylist)
+                  (cons (cons (car xlist) (car ylist))
+                        (f (cdr xlist) (cdr ylist))))])
+    (lambda () (f xs ys))))
+
+
+(define (cycle-lists xs ys)
+  (letrec ([f (lambda (xlist ylist)
+                  (cons (cons (if (null? xlist) (car xs) (car xlist))
+                              (if (null? ylist) (car ys) (car ylist)))
+                        (lambda () (f (if (null? xlist) (cdr xs) (cdr xlist))
+                                      (if (null? ylist) (cdr ys) (cdr ylist))))))])
+    (lambda () (f xs ys))))
+
+
 
 
 (define ones (lambda () (cons 1 ones)))
